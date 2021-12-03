97b8d86f6acb48dae29e5074e05d5ec3ceafa1f9
Tae-HyenAhn <thahn88@naver.com>
2021-12-03 17:53:11 +0900

    test

--- libs/reservation.js
+++ libs/reservation.js
@@ -7,7 +7,8 @@ export function getReservationList(params, token) {
     return axios({
         method: 'get',
         url: '/reservation/list?start_no=0&row=10&start_date=2021-01-01&end_date=2021-12-31',
-        baseURL: baseUrl[env].baseUrl,
+        // baseURL: baseUrl[env].baseUrl,
+        baseURL: 'https://rooming.link/api',
         // params: params,
         headers: {
             'Authorization': 'Bearer '+'21|s5ScUWoQAMNnhmZnl2enMPmnekqTJlLBczzUMXZV'
